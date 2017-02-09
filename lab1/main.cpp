8d9fb66fde8fc601bd39271c3b3682843cd172dc
Ben Patient <bpatient24@gmail.com>
2017-02-09 00:27:33 -0800

    Initial Commit

---
+++ lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by Ben Patient on 2/9/17.
+//  Copyright © 2017 Ben Patient. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
